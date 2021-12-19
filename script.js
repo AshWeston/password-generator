29588990f90cd3f473a15549e2691f47551f9a0a
Ash Weston <ash-w@live.com.au>
2021-12-19 15:25:05 +1000

    created function for generatePassword and user prompts

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +13,16 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// Prompts when "Generate Password" is clicked
+function generatePassword() {
+  var passwordLength = prompt("Please choose a character length for your password.  It must be greater than 12 but less than 128.");
+
+  var lowerCases = confirm("Do you want to include lowercase letters?");
+
+  var upperCases = confirm("Do you want to include uppercase letters?");
+
+  var numbers = confirm("Do you want to include numbers?");
+
+  var special = confirm("Do you want to include special characters?");
