9b52ab8db1581564df54e35c866e58974392cf6c
Ash Weston <ash-w@live.com.au>
2021-12-23 16:31:02 +1000

    final update to script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ var specialCharacters = "!@#$%^&*()";
 
 // Prompts when "Generate Password" is clicked
 function generatePassword() {
-  var passwordLength = prompt("Please choose a character length for your password.  It must be greater than 12 but less than 128.");
+  var passwordLength = prompt("Please choose a character length for your password.  It must be greater than 8 but less than 128.");
 
   var lowerCases = confirm("Do you want to include lowercase letters?");
 
